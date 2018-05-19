3b2039230a8163dbe410aac563889c9d8f7332a6
KitKat91 <djproject021@gmail.com>
2018-05-19 18:55:00 +0300

    mokko

---
+++ sites/mokko/js/common.js
@@ -0,0 +1,67 @@
+$(function() {
+
+	// Custom JS
+
+});
+function increaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value++;
+  document.getElementById('number').value = value;
+}
+
+function decreaseValue() {
+  var value = parseInt(document.getElementById('number').value, 10);
+  value = isNaN(value) ? 0 : value;
+  value < 1 ? value = 1 : '';
+  value--;
+  document.getElementById('number').value = value;
+}
+
+$(function(){
+  $(".nav__icon").on("click", function() {
+    $(this).toggleClass("active");
+    $("nav ul").slideToggle();
+  });
+
+  $(window).resize(function(){
+    var w = $(window).width();
+    var h = $(window).height();
+    var x = 768;
+    if (w >= x) {
+      $('nav ul').css({ display: 'flex',height: 'auto' });
+    }else {
+      $('nav ul').css({ display: 'none',height: h + 'px'});
+    }
+  });
+});
+
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
